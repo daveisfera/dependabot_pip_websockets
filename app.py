76b57f87928ecc077605a93f4ffb6643a6a7572e
Dave Johansen <davejohansen@gmail.com>
2025-01-16 12:51:18 -0700

    Add logging to verify it's working

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ from websockets.asyncio.server import serve
 async def handler(websocket):
     while True:
         message = await websocket.recv()
-        print(message)
+        print(message, flush=True)
 
 
 async def main():
+    print("Starting", flush=True)
     async with serve(handler, "", 8001):
         await asyncio.get_running_loop().create_future()  # run forever
+    print("Done", flush=True)
 
 
 if __name__ == "__main__":
